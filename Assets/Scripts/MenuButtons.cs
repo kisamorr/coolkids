8790bf68c5cd39c6e2161adf8daeebf727c872ab
Margaret Kennedy <megkendev@gmail.com>
2023-10-31 15:44:27 -0400

    UI system and inventory finalized

---
+++ Assets/Scripts/MenuButtons.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuButtons : MonoBehaviour
+{
+    public Button exitButton;
+    public Button restartButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        exitButton.onClick.AddListener(exitGame);
+        restartButton.onClick.AddListener(restartGame);
+    }
+
+    public void restartGame()
+    {
+        SceneManager.LoadScene("SampleScene");
+        GameManager.instance.InventorySystem.SetActive(false);
+    }
+
+    public void exitGame()
+    {
+        Application.Quit();
+    }
+}
