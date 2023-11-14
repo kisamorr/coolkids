76535435c16bd38445727ee56a0cdc0d72f705e6
Margaret Kennedy <megkendev@gmail.com>
2023-11-14 13:53:41 -0500

    added notes + collectibles throughout scene in prep for argument

--- Assets/Scripts/Note.cs
+++ Assets/Scripts/Note.cs
@@ -5,16 +5,16 @@ using UnityEngine.UI;
 
 public class Note : MonoBehaviour
 {
-    public static Note instance;
+    //public static Note instance;
     public NoteItem note;
 
-    private void Awake()
+    /*private void Awake()
     {
         if (instance == null)
         {
             instance = this;
         }
-    }
+    }*/
 
     public void ObtainNote()
     {
