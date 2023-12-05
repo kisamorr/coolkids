46ab859761057e90e53d9dccb4a066fa745935b5
Margaret Kennedy <megkendev@gmail.com>
2023-12-05 15:25:57 -0500

    harper triggers fixed in skies level, added a task manager based off of prior playtest feedback

--- Assets/Scripts/MomShowUp.cs
+++ Assets/Scripts/MomShowUp.cs
@@ -6,6 +6,7 @@ public class MomShowUp : MonoBehaviour
 {
     public StoryTrigger titaTrigger, guysTrigger, mariaTrigger;
     public GameObject momObject;
+    public GameObject task1, task2, task3, task4;
 
     // Update is called once per frame
     void Update()
@@ -13,6 +14,10 @@ public class MomShowUp : MonoBehaviour
         if (titaTrigger.dialogueFinished && guysTrigger.dialogueFinished && mariaTrigger.dialogueFinished)
         {
             momObject.SetActive(true);
+            task1.SetActive(false);
+            task2.SetActive(false);
+            task3.SetActive(false);
+            task4.SetActive(true);
         }
     }
 }
--- Assets/Scripts/Story/Harper Scripts/EndHarperTrigger.cs
+++ Assets/Scripts/Story/Harper Scripts/EndHarperTrigger.cs
@@ -2,14 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HarperTriggerFinal : MonoBehaviour
+public class EndHarperTrigger : MonoBehaviour
 {
     public GameObject Harper;
-    public HarperTrigger2 harperTrigger2;
 
     private void OnTriggerEnter(Collider collider)
     {
-        if (harperTrigger2.isActivated && collider.gameObject.tag == "Player")
+        if (collider.gameObject.tag == "Player")
         {
             Harper.SetActive(true);
         }
